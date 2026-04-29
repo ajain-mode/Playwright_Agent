14c3a00af215fbce4096c76ff12d21950bbf1f96
Rohit Singh <rohit.singh@modeglobal.com>
2026-04-29 23:28:05 +0530

    Billing Toggle Yaml Added

--- src/testmo/testmo-reporter.ts
+++ src/testmo/testmo-reporter.ts
@@ -3,7 +3,7 @@ import * as fs from "fs";
 import * as path from "path";
 import { execSync } from "child_process";
 import commonReusables from "@utils/commonReusables";
-import loginSetup from "src/loginHelpers/loginSetup";
+import loginSetup from "@loginHelpers/loginSetup";
 class TestmoReporter implements Reporter {
   private results: any[] = [];
 
