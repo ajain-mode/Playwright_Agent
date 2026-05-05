71de776c8a0d4dd2b50ddaa3749ba5253b997074
aghaytad-mode <akshada.ghaytadkar@modeglobal.com>
2026-05-05 16:10:04 +0530

    Added Locator

--- src/pages/tnx/TNXLandingPage.ts
+++ src/pages/tnx/TNXLandingPage.ts
@@ -60,7 +60,7 @@ class TNXLandingPage {
       "//*[text()='1 Available Loads']"
     );
     this.loadOfferRateValue_LOC = page.locator(
-      "//*[contains(@class,'Tender__price')]//div[contains(@class,'Level__level')]//p"
+      "//*[contains(@class,'Tender__price')]//p[contains(@class,'Typography__is-primary') and contains(@class,'Typography__is-bold')]"
     );
     this.temporaryDelayPopUp_LOC = page.locator("//*[text()='Got it!']");
     this.availableJobsText_LOC = page.locator(
